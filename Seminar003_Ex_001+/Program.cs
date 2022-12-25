216b96ef294488883dce657a98e15d6d249b8c16
IgorMarokhin <lawyer.mii222@gmail.com>
2022-12-25 16:07:13 +0300

    Добавлен проект: решение задачи 17 с использованием методов

---
+++ Seminar003_Ex_001+/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Clear();
+int X = 0, Y = 0;
+InitCoordinatesByUser(ref X, ref Y);
+PrintQuarterByCoords(X, Y);
+
+static void InitCoordinatesByUser(ref int x, ref int y)
+{
+    try
+    {
+        Console.Write("Введите X: ");
+        x = int.Parse(Console.ReadLine() ?? "");
+
+        Console.Write("Введите Y: ");
+        y = int.Parse(Console.ReadLine() ?? "");
+    }
+    catch(Exception exc)
+    {
+        Console.WriteLine($"Ошибка ввода данных! {exc.Message}");
+        return;
+    }
+}
+
+static void PrintQuarterByCoords(int x, int y)
+{
+    if (x > 0 && y > 0)
+        Console.WriteLine("1");
+    else if (x < 0 && y > 0)
+        Console.WriteLine("2");
+    else if (x < 0 && y < 0)
+        Console.WriteLine("3");
+    else if (x > 0 && y < 0)
+        Console.WriteLine("4");
+    else
+        Console.WriteLine("Ошибка! Точка попадает на оси координат!");
+}
