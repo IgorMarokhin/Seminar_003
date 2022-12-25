c459e283f35dabfdb0b085685f2b94e0dd9eef59
IgorMarokhin <lawyer.mii222@gmail.com>
2022-12-25 21:09:34 +0300

    Добавлено решение домашнего задания: задача 23

---
+++ Seminar003_HW_003/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите целое число: ");
+int N = int.Parse(Console.ReadLine() ?? "");
+
+for (int count = 1; count <= N; count++)
+{
+    Console.Write($"{count * count * count}, ");
+}
+Console.WriteLine(".");
