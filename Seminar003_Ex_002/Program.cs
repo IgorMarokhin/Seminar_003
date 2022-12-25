e79a0f6c61cef42c3c8a8bc9bdd3b2a6d1a8fe22
IgorMarokhin <lawyer.mii222@gmail.com>
2022-12-25 16:19:23 +0300

    Добавлен проект: решение задачи 18

---
+++ Seminar003_Ex_002/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Введите номер четверти (от 1 до 4): ");
+int num = int.Parse(Console.ReadLine() ?? "");
+
+if (num == 1)
+    Console.WriteLine("X > 0, Y > 0");
+else if (num == 2)
+    Console.WriteLine("X < 0, Y > 0");
+else if (num == 3)
+    Console.WriteLine("X < 0, Y < 0");
+else if (num == 4)
+    Console.WriteLine("X > 0, Y < 0");
+else
+    Console.WriteLine("Ошибка! Введено не корректное число!");
