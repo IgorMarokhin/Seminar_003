fe673c1f362dbe9fa9731919033f6aa8fad62f30
IgorMarokhin <lawyer.mii222@gmail.com>
2022-12-26 11:39:18 +0300

    Добавлено решение домашнего задания: задача 23

--- Seminar003_HW_003/Program.cs
+++ Seminar003_HW_003/Program.cs
@@ -1,8 +1,19 @@
-﻿Console.Write("Введите целое число: ");
-int N = int.Parse(Console.ReadLine() ?? "");
+﻿int N;
 
-for (int count = 1; count <= N; count++)
+try
 {
-    Console.Write($"{count * count * count}, ");
+    Console.Write("Введите целое число: ");
+    N = int.Parse(Console.ReadLine() ?? "");
 }
-Console.WriteLine(".");
+catch(Exception exc)
+{
+    Console.WriteLine($"Ошибка ввода данных! {exc.Message}");
+    return;
+}
+Console.Write($"{N} -> ");
+
+for (int count = 1; count < N; count++)
+{
+    Console.Write($"{Math.Pow(count,3)}, ");
+}
+Console.WriteLine($"{Math.Pow(N,3)}. ");
